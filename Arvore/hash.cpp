922d4a6e1b813fe63689f0774065e8dd977aef7b
Lafalle <124217154+Lafalle@users.noreply.github.com>
2023-07-03 12:18:50 -0300

    Update hash.cpp

--- Arvore/hash.cpp
+++ Arvore/hash.cpp
@@ -11,15 +11,14 @@ using std::vector;
 template <typename HashedObj>
 class HashTable {
     public:
-        explicit HashTable(int size = 101) : array(nextPrime(size)) { 
+        explicit HashTable(int size = 10007) : array(nextPrime(size)) { 
             makeEmpty(); //Cria uma tabela hash marcada como vazia
-            printf("Tabela criada\n");
             }
-
+            
         bool contains(const HashedObj & x) const { 
             return isActive(findPos(x)); //Verifica pelo elemento x na tabela hash ativa
             }
-
+            
         void makeEmpty( ) {
             currentSize = 0; //Inicializa com o tamanho atual de 0
             
@@ -27,29 +26,24 @@ class HashTable {
                 entry.info = EMPTY; //Marca todos os compartimentos como vazios
             }
         }
-
+        
         bool insert(const HashedObj & x) {
             int currentPos = findPos(x); //Verifica se o elemento x está presente como ativo
             
             if (isActive(currentPos)) {
-                //printf("Elemento atualmente ativo na Tabela.\n");
                 return false; //Retorna falso se já estiver ativo
             }
             
-            //printf("Elemento %d, adicionado no compartimento %d.\n", x, currentPos);
             array[currentPos].element = x; //Insere o elemento x
             array[currentPos].info = ACTIVE; //Marca o elemento x como ativo
-
+            
             if (++currentSize > array.size() / 2) {
-                //printf("Fator de carga maior que 50 porcento, iniciando rehash.\n");
                 rehash(); //Caso o fator de carga exceda 50%, redimensiona a tabela
             }
             
             return true; //Retorna verdadeiro se inserção foi bem sucedida
         }
-
-        //bool insert(HashedObj && x);
-
+        
         bool remove(const HashedObj & x) {
             int currentPos = findPos(x); //Verifica se o elemento x está presente como ativo
             
@@ -71,51 +65,38 @@ class HashTable {
             int currentPos = myhash(x);
             
             while (array[currentPos].info != EMPTY && array[currentPos].element != x) { //Procura pela posição disponivel para inserir o elemento x
-                //printf("Colisão detectada, iniciando metódo quadrático\n");
                 currentPos += offset; //Computa a próxima posição usando colisão quadrática
                 offset += 2; //Incrementa o deslocamento para a próxima tentativa de busca
                 
                 if (currentPos >= array.size()) {
                     currentPos -= array.size(); //Caso a posição atual supere o tamanho total, subtraimos o tamanho para retorna ao começo
                 }
-            //printf("Metódo quadrático finalizado\n");
             }
             
             
             return currentPos; //Retorna a posição em que inserir o elemento ou a posição onde o elemento foi encontrado
         }
         
-        void printHash(const HashedObj) {
-            printf("Realizando impressão dos elementos na tabela: \n");
-            for (int i = 0; i == array.size; i++) {
-                printf("%d ", array[i].element);
-            }
-        }
-        
-        
     private:
         struct HashEntry {
             HashedObj element;
             EntryType info;
-
             HashEntry(const HashedObj & e = HashedObj{}, EntryType i = EMPTY)
                 : element{e}, info{i}{}
             HashEntry(HashedObj && e, EntryType i = EMPTY)
                 : element{std::move(e)}, info{i} {}
         };
-
+        
         vector<HashEntry> array;
         int currentSize;
-
+        
         bool isActive(int currentPos) const {
             return array[currentPos].info == ACTIVE; //Verifica se o compartimento solicitado está ativo
         }
-
         
-
         void rehash() {
             vector<HashEntry> oldArray = array;
-
+            
             array.resize(nextPrime(2 * oldArray.size())); //Cria uma nova tabela vazia, com dobro do tamanho
             for (auto & entry : array) {
                 entry.info = EMPTY;
@@ -128,7 +109,7 @@ class HashTable {
                 }
             }
         }
-
+        
         size_t myhash( const HashedObj & x ) const {
             static hash<HashedObj> hf;
             return hf(x) % array.size(); // Aplica a função de hash ao elemento x e retorna o índice correspondente
