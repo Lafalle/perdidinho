c32b9feb1b6161d79d81b22bfaf1da526cadcb1e
Lafalle <124217154+Lafalle@users.noreply.github.com>
2023-07-03 12:17:20 -0300

    Update main.cpp

--- Arvore/main.cpp
+++ Arvore/main.cpp
@@ -7,19 +7,71 @@ int main (){
     std::random_device rd;
     std::mt19937 gen(rd());
     // Crie uma distribuição uniforme para gerar números inteiros entre um intervalo
-    std::uniform_int_distribution<int> dist(1, 100000);
+    std::uniform_int_distribution<int> dist(1, 10000);
 
     int num = dist(gen);
     HashTable<int> tabela;
     RedBlackTree arvore;
     
+    
     auto startTimeHash = std::chrono::high_resolution_clock::now();
-    tabela.contains(num);
+    // Insira números aleatórios na tabela hash
+    for (int i = 0; i <= 1000000; ++i) {
+       int num = dist(gen);  // Gera um número aleatório usando o gerador e a distribuição
+       tabela.insert(num);
+    }
     auto endTimeHash = std::chrono::high_resolution_clock::now();
     auto durationHash = std::chrono::duration_cast<std::chrono::nanoseconds>(endTimeHash - startTimeHash);
-    std::cout << "Tempo de busca na Hash: " << durationHash.count() << " nanossegundos" << std::endl;
+    durationHash = durationHash / 1000000;
+    std::cout << "\nTempo de inserção (de 1000000 itens) na Hash: " << durationHash.count() << " milissegundos\n" << std::endl;
     
-    arvore.search(num);
+    
+    startTimeHash = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i <= 100000; i++) {
+        int num = dist(gen);  // Gera um número aleatório usando o gerador e a distribuição
+        
+        if (tabela.contains(num)) {
+            //std::cout << "A tabela contem o número " << num << std::endl;
+        } 
+        
+        else {
+            //std::cout << "A tabela nao contem o número " << num << std::endl;
+        }
+    }
+    endTimeHash = std::chrono::high_resolution_clock::now();
+    durationHash = std::chrono::duration_cast<std::chrono::nanoseconds>(endTimeHash - startTimeHash);
+    durationHash = durationHash / 1000000;
+    std::cout << "\nTempo de busca (por 100000 itens) na Hash: " << durationHash.count() << " milissegundos\n" << std::endl;
+    
+    
+    auto startTimeTree = std::chrono::high_resolution_clock::now();
+    // Insira números aleatórios na tabela hash
+    for (int i = 0; i <= 1000000; ++i) {
+       int num = dist(gen);  // Gera um número aleatório usando o gerador e a distribuição
+       arvore.search(num);
+    }
+    auto endTimeTree = std::chrono::high_resolution_clock::now();
+    auto durationTree = std::chrono::duration_cast<std::chrono::nanoseconds>(endTimeTree - startTimeTree);
+    durationTree = durationTree / 1000000;
+    std::cout << "\nTempo de inserção (de 1000000 itens) na Árvore: " << durationHash.count() << " milissegundos\n" << std::endl;
+    
+    
+    startTimeTree = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i <= 100000; i++) {
+        int num = dist(gen);  // Gera um número aleatório usando o gerador e a distribuição
+        
+        if (arvore.search(num)) {
+            //std::cout << "A árvore contem o número " << num << std::endl;
+        }
+        
+        else {
+            //std::cout << "A árvore contem o número " << num << std::endl;
+        }
+    }
+    endTimeTree = std::chrono::high_resolution_clock::now();
+    durationTree = std::chrono::duration_cast<std::chrono::nanoseconds>(endTimeTree - startTimeTree);
+    durationTree = durationTree / 1000000;
+    std::cout << "\nTempo de busca (por 100000 itens) na Árvore: " << durationHash.count() << " milissegundos\n" << std::endl;
 
 
     return 0;
